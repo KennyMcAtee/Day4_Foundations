dc47bf2197de23d2f6d5bf2aa970f7598946729e
KennyMcAtee <mcateekenny@gmail.com>
2017-02-17 20:05:58 -0500

    Day 4

---
+++ Day4.js
@@ -0,0 +1,26 @@
+var basket = 1;
+console.log(basket);
+
+basket= 2;
+console.log(basket);
+
+var favAnimal = "llama"
+console.log(favAnimal);
+
+favAnimal = "river otter";
+console.log(favAnimal);
+
+var name = "kenny";
+console.log(name);
+console.log(name);
+console.log(name);
+
+console.log(((10 + 12) * (31- 4)) / 2);
+
+var car = "my broken car ";
+var carVerb = "broke down ";
+var driverVerb = "I fixed ";
+var past = "yesterday ";
+var present = "today ";
+
+console.log(car + carVerb + driverVerb + past + present);
